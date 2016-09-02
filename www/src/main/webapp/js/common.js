f4a9db9dedfeeb4995b4c11523b5f0063f6e5cda
asinoai <aron.sinoai@linkonomics.com>
2016-09-02 12:13:46 +0300

    Changing the menu into nav; still not working in small width, because of white text on white background

--- www/src/main/webapp/js/common.js
+++ www/src/main/webapp/js/common.js
@@ -4,7 +4,7 @@ var helloit = helloit || {};
 helloit.createHeader = function(title, subTitle) {
 
     return $('<div class="row header"></div>')
-        .append($('<div class="logo"></div>'))
+        .append($('<div class="logo"><div></div></div>'))
         .append($('<div class="title"></div>')
             .append(title)
         )
@@ -18,12 +18,28 @@ helloit.createHeader = function(title, subTitle) {
 helloit.createNavbar = function () {
 
     return $('\
-    <nav class="navbar navbar-light bg-faded main-navbar hidden-xl-down">\
+    <div class="row">\
+    <div class="col-md-3"></div>\
+    <nav class="navbar navbar-light col-md-9 menu">\
         <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">\
             &#9776;\
         </button>\
         <div class="collapse navbar-toggleable-xs" id="exCollapsingNavbar2">\
+          <ul class="nav navbar-nav">\
+            <li class="nav-item menuitem">\
+              <a class="nav-link" href="index.html">Junior Java WEB programozó képzés</a>\
+            </li>\
+            <li class="nav-item menuitem">\
+              <a class="nav-link" href="#">Kapcsolat</a>\
+            </li>\
+            <li class="nav-item menuitem">\
+              <a class="nav-link" href="#">Galéria</a>\
+            </li>\
+            <li class="nav-item menuitem">\
+              <a class="nav-link" href="#">Rólunk</a>\
+            </li>\
+          </ul>\
         </div>\
-    </nav>');
+    </nav>\
+    ');
 };
-
