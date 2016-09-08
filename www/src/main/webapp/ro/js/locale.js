0957ad935c26cbeb39e596afdef8409fb2ef381a
asinoai <aron.sinoai@linkonomics.com>
2016-09-08 13:16:26 +0300

    Small typo

--- www/src/main/webapp/ro/js/locale.js
+++ www/src/main/webapp/ro/js/locale.js
@@ -3,7 +3,7 @@ var helloit = helloit || {};
 helloit.i18n = {
     course1Title : 'Junior Java',
     course1SubTitle : 'Curs programare WEB',
-    course1FullTitle : 'Curs programare WEB, Junior Java',
+    course1FullTitle : 'Curs programare WEB - Junior Java',
     contact : 'Contact',
     gallery : 'Galerie foto',
     aboutUs : 'Despre noi'
