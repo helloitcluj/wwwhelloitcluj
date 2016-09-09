10be3c968ab1f26083093106cd4ef992355bb52a
asinoai <aron.sinoai@linkonomics.com>
2016-09-09 17:55:09 +0300

    Locale ro update

--- www/src/main/webapp/ro/js/locale.js
+++ www/src/main/webapp/ro/js/locale.js
@@ -8,21 +8,21 @@ helloit.i18n = {
     gallery : 'Galerie foto',
     aboutUs : 'Despre noi',
     course1Description : $('<div class="contentheader1">Curs programare WEB - Junior Java</div>\
-        <div class="contentheader2">Milyen tudást ad a képzés?</div>\
-        <div class="content">A képzés a programozás alapjait, a web programozáshoz általános és egy ismert, gyakran használt technológiájának elméleti és gyakorlati információit adja át.</div>\
-        <div class="content">A tudás hasznossága kb egy év munkahelyi tapasztalattal ér fel. Amennyi tapasztalatot tudnál szerezni egy év alatt, nulláról indulva, de a projektre koncentrálva. Itt miután az egyéni fejlődésedre figyelünk ezeket az ismereteket gyorsabban megtanulhatod.</div>\
+        <div class="contentheader2">Ce oferã cursul?</div>\
+        <div class="content">La curs se poate învãţa bazele programãrii, o technologie specificã, dar foarte utilizatã a programárii WEB, dar şi bazele şi teoria acestuia.</div>\
+        <div class="content">Cunoştiinţele acumulate corespund cu experienţa de un an la un loc de muncã, unde evoluţia proiecului este obiectul, iar la noi evoluţia ta. Avem grijã de fiecare personã, astfel cunoştinţele pot fi adunate, învãţate mai repede.</div>\
 \
-        <div class="contentheader2">Előképzettség?</div>\
-        <div class="content">A képzés nem igényel előzetes programozási tudást. Logikus gondolkodás viszont segít. Elvárjuk, hogy a képzést komolyan vegyétek, ne lógjatok az órákról.</div>\
+        <div class="contentheader2">Cunoştiinţe premilinare?</div>\
+        <div class="content">Participarea la training nu cere nici o formare tradinitoanalã in avans, dar gândirea logicã constituie un avantaj. Prezenţa la majoritatea orelor este obligatorie.</div>\
 \
-        <div class="contentheader2">Időtartam</div>\
-            <div class="content">Szeptember 5-én kezdődik és 160 órát tart. Hétfőn, szerdán és csütörtön délután 6-tól. </div>\
+        <div class="contentheader2">Perioada</div>\
+            <div class="content">Începe la 5 septembrie și va dura 160 de ore . Luni, miercuri și joi de la 6 seara.</div>\
 \
-        <div class="contentheader2">Tandíj</div>\
-            <div class="content">A képzés ára 750 euro és 3 részletben fizethető, minden hónap elején. Amennyiben hozol még csapattársakat, akkor adunk kedvezményt, minden csapattárs után 100 euro-val kell kebesebbet fizetned. Egy csoportban max 20 fő lehet.</div>\
+        <div class="contentheader2">Preţul</div>\
+            <div class="content">Preţul acestui curs este 1000 euro şi poate fi plãtit in trei rate, la începutul fiecãrui luni. Dacã aduci alţi participanţi la curs îţi dãm o reducere de 100 de euro pentru fiecare personã. În fiecare grupã pot fi maxim 20 de persoane.</div>\
 \
-        <div class="contentheader2">Hogyan műkődik a gyakorlatban?</div>\
-        <div class="content">A képzés a gyakorlatra alapul és mindenkire személyesen külön figyelünk. Mindennek elmondjuk az elméleti hátterét, de ki is próbálja mindenki gyakorlatban is, kódot írva. A munkahelyi körülményekhez hasonlóan itt is csapatban “dolgoztok”, hogy </div>\
+        <div class="contentheader2">Cum funcţioneazã în realitate?</div>\
+        <div class="content">Cursul este bazat pe practicã şi avem atenţie pentru fiecare participant separat. Prezentãm teoria a fiecãrui element predat, dar totul este încercat în practicã, scriind cod. Ca şi la locul de muncã şi la noi se “lucreazã" în echipã.</div>\
     '),
     registrationTitle : "Înscriere",
     nameField : "nume",
@@ -31,7 +31,9 @@ helloit.i18n = {
     uploadCVField : "încãrcare CV",
     registrationButton : "ÎNSCRIERE",
     contactDescription : $('<div class="contentheader1">Contact</div>'),
-    aboutUsDescription : $('<div class="contentheader1">Despre noi</div>'),
+    aboutUsDescription : $('<div class="contentheader1">Despre noi</div>\
+        <div class="content">Lucrãm mai mult de 10 ani in industria IT, şi la firme mici şi la multinaţionali. Am învãţat şi am folosit technologii care deja sunt mai puţin folosite, dar folosim de mulţi ani technologiile care sunt cele mai rãspândite în aceste zile. În ultimii ani am introdus mulţi începãtori în tainele programãrii, am ţinut mai multe sute de interviuri. Având în vedere cât de mult se cautã programatori cu experienţã utilã am decis sã ne ocupãm cu acesta ca şi activitate principlã.</div>\
+        <div class="content">Cursurile se ţin în sãlile de curs închiriate de la Universitatea Sapientia în Cluj-Napoca, str Matei Corvin nr 4. Cursanţii învaţã pe laptopurile noastre şi bine înţeles pentru a ajuta la menţinerea atenţiei cafea şi apã este la dispoziţia voastrã.</div>'),
     galleryDescription : $('<div class="contentheader1">Galerie foto</div>')
 
 
